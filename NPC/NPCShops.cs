77964749d2d0038e8ab555722cabac484a4a283e
Pannoniae <prisznyak.zsombor@gmail.com>
2023-03-25 16:55:20 +0000

    - implement proper conditions for npc shops

--- NPC/NPCShops.cs
+++ NPC/NPCShops.cs
@@ -14,6 +14,7 @@ public class NPCshop : GlobalNPC {
         if (Main.hardMode) {
             cost *= 2;
         }
+
         Recipe.FindRecipes();
 
         if (NPC.downedMoonlord) {
@@ -26,87 +27,87 @@ public class NPCshop : GlobalNPC {
                     Item.buyPrice(0, 0, 0, 20));
                 addToShop(shop, ItemID.WormholePotion,
                     Item.buyPrice(0, 0, 25, 0));
-                addToShop(shop, ItemID.ArcheryPotion,
-                    Item.buyPrice(0, cost, 0, 0), () => NPC.downedBoss1);
-                addToShop(shop, ItemID.HealingPotion, cond: () => NPC.downedBoss2);
-                addToShop(shop, ItemID.ManaPotion, cond: () => NPC.downedBoss2);
-                addToShop(shop, ItemID.TitanPotion,
-                    Item.buyPrice(0, 2, 0, 0), () => NPC.downedBoss3);
-                addToShop(shop, ItemID.ApprenticeBait, cond: () => NPC.downedBoss1);
-                addToShop(shop, ItemID.JourneymanBait, cond: () => NPC.downedBoss3);
+                addToShop(shop, ItemID.ArcheryPotion, Condition.DownedEyeOfCthulhu,
+                    Item.buyPrice(0, cost, 0, 0));
+                addToShop(shop, ItemID.HealingPotion, Condition.DownedEowOrBoc);
+                addToShop(shop, ItemID.ManaPotion, Condition.DownedEowOrBoc);
+                addToShop(shop, ItemID.TitanPotion, Condition.DownedSkeletron,
+                    Item.buyPrice(0, 2, 0, 0));
+                addToShop(shop, ItemID.ApprenticeBait, Condition.DownedEyeOfCthulhu);
+                addToShop(shop, ItemID.JourneymanBait, Condition.DownedSkeletron);
                 // haha funny
-                addToShop(shop, ItemID.MasterBait, cond: () => NPC.downedPlantBoss);
+                addToShop(shop, ItemID.MasterBait, Condition.DownedPlantera);
                 addToShop(shop, ItemID.Burger, Item.buyPrice(0, 5, 0, 0),
                     () => NPC.downedBoss3);
 
-                addToShop(shop, ItemID.Hotdog,
-                    Item.buyPrice(0, 5, 0, 0), () => Main.hardMode);
+                addToShop(shop, ItemID.Hotdog, Condition.Hardmode,
+                    Item.buyPrice(0, 5, 0, 0));
                 addToShop(shop, ItemID.CoffeeCup, Item.buyPrice(0, 2, 0, 0));
                 break;
             case NPCID.Demolitionist:
-                addToShop(shop, ItemID.MiningPotion,
-                    Item.buyPrice(0, 1, 0, 0), () => NPC.downedBoss1);
-                addToShop(shop, ItemID.IronskinPotion,
-                    Item.buyPrice(0, 1, 0, 0), () => NPC.downedBoss1);
-                addToShop(shop, ItemID.ShinePotion,
-                    Item.buyPrice(0, 1, 0, 0), () => NPC.downedBoss1);
-                addToShop(shop, ItemID.SpelunkerPotion,
-                    Item.buyPrice(0, 2, 0, 0), () => NPC.downedBoss2);
-                addToShop(shop, ItemID.ObsidianSkinPotion,
-                    Item.buyPrice(0, 2, 0, 0), () => NPC.downedBoss3);
-                addToShop(shop, ItemID.EndurancePotion,
-                    Item.buyPrice(0, cost, 0, 0), () => NPC.downedBoss3);
+                addToShop(shop, ItemID.MiningPotion, Condition.DownedEyeOfCthulhu,
+                Item.buyPrice(0, 1, 0, 0));
+                addToShop(shop, ItemID.IronskinPotion, Condition.DownedEyeOfCthulhu,
+                    Item.buyPrice(0, 1, 0, 0));
+                addToShop(shop, ItemID.ShinePotion, Condition.DownedEyeOfCthulhu,
+                    Item.buyPrice(0, 1, 0, 0));
+                addToShop(shop, ItemID.SpelunkerPotion, Condition.DownedEowOrBoc,
+                    Item.buyPrice(0, 2, 0, 0));
+                addToShop(shop, ItemID.ObsidianSkinPotion, Condition.DownedSkeletron,
+                    Item.buyPrice(0, 2, 0, 0));
+                addToShop(shop, ItemID.EndurancePotion, Condition.DownedSkeletron,
+                    Item.buyPrice(0, cost, 0, 0));
                 break;
             case NPCID.DyeTrader:
                 addToShop(shop, ItemID.DyeTradersScimitar, price: Item.buyPrice(0, 15, 0, 0));
                 break;
             case NPCID.ArmsDealer:
-                addToShop(shop, ItemID.Boomstick,
-                    Item.buyPrice(0, 20, 0, 0), () => NPC.downedQueenBee);
-                addToShop(shop, ItemID.AmmoBox,
-                    Item.buyPrice(0, 25, 0, 0), () => Main.hardMode);
-                addToShop(shop, ItemID.Uzi,
-                    Item.buyPrice(0, 45, 0, 0), () => NPC.downedPlantBoss);
-                addToShop(shop, ItemID.TacticalShotgun,
-                    Item.buyPrice(0, 60, 0, 0), () => NPC.downedGolemBoss);
-                addToShop(shop, ItemID.SniperRifle,
-                    Item.buyPrice(0, 60, 0, 0), () => NPC.downedGolemBoss);
-                addToShop(shop, ItemID.RifleScope,
-                    Item.buyPrice(0, 60, 0, 0), () => NPC.downedGolemBoss);
+                addToShop(shop, ItemID.Boomstick, Condition.DownedQueenBee,
+                    Item.buyPrice(0, 20, 0, 0));
+                addToShop(shop, ItemID.AmmoBox, Condition.Hardmode,
+                    Item.buyPrice(0, 25, 0, 0));
+                addToShop(shop, ItemID.Uzi, Condition.DownedPlantera,
+                    Item.buyPrice(0, 45, 0, 0));
+                addToShop(shop, ItemID.TacticalShotgun, Condition.DownedGolem,
+                    Item.buyPrice(0, 60, 0, 0));
+                addToShop(shop, ItemID.SniperRifle, Condition.DownedGolem,
+                    Item.buyPrice(0, 60, 0, 0));
+                addToShop(shop, ItemID.RifleScope, Condition.DownedGolem,
+                    Item.buyPrice(0, 60, 0, 0));
                 addToShop(shop, ItemID.AmmoReservationPotion,
                     Item.buyPrice(0, 1, 0, 0));
                 addToShop(shop, ItemID.HunterPotion,
                     Item.buyPrice(0, 2, 0, 0));
-                addToShop(shop, ItemID.BattlePotion,
-                    Item.buyPrice(0, 2, 0, 0), () => NPC.downedBoss2);
+                addToShop(shop, ItemID.BattlePotion, Condition.DownedEowOrBoc,
+                    Item.buyPrice(0, 2, 0, 0));
                 break;
             case NPCID.Stylist:
                 addToShop(shop, ItemID.ChocolateChipCookie,
                     Item.buyPrice(0, 3, 0, 0));
                 break;
             case NPCID.Cyborg:
-                addToShop(shop, ItemID.RocketLauncher,
-                    Item.buyPrice(0, 25, 0, 0), () => NPC.downedGolemBoss);
+                addToShop(shop, ItemID.RocketLauncher, Condition.DownedGolem,
+                    Item.buyPrice(0, 25, 0, 0));
                 break;
             case NPCID.Dryad:
                 addToShop(shop, ItemID.ThornsPotion,
                     Item.buyPrice(0, 1, 0, 0));
                 addToShop(shop, ItemID.FeatherfallPotion,
                     Item.buyPrice(0, 1, 0, 0));
-                addToShop(shop, ItemID.RegenerationPotion,
-                    Item.buyPrice(0, 2, 0, 0), () => NPC.downedBoss2);
-                addToShop(shop, ItemID.SwiftnessPotion,
-                    Item.buyPrice(0, cost, 0, 0), () => NPC.downedBoss2);
+                addToShop(shop, ItemID.RegenerationPotion, Condition.DownedEowOrBoc,
+                    Item.buyPrice(0, 2, 0, 0));
+                addToShop(shop, ItemID.SwiftnessPotion, Condition.DownedEowOrBoc,
+                    Item.buyPrice(0, cost, 0, 0));
                 addToShop(shop, ItemID.JungleRose, Item.buyPrice(0, 2, 0, 0));
                 addToShop(shop, ItemID.NaturesGift, Item.buyPrice(0, 10, 0, 0));
-                addToShop(shop, ItemID.Grapes,
-                    Item.buyPrice(0, 2, 50, 0), () => NPC.downedBoss3);
+                addToShop(shop, ItemID.Grapes, Condition.DownedSkeletron,
+                    Item.buyPrice(0, 2, 50, 0));
                 break;
             case NPCID.GoblinTinkerer:
                 addToShop(shop, ItemID.StinkPotion,
                     Item.buyPrice(0, 1, 0, 0));
-                addToShop(shop, ItemID.Spaghetti,
-                    Item.buyPrice(0, 5, 0, 0), () => NPC.downedBoss3);
+                addToShop(shop, ItemID.Spaghetti, Condition.DownedSkeletron,
+                    Item.buyPrice(0, 5, 0, 0));
                 break;
             case NPCID.Mechanic:
                 addToShop(shop, ItemID.BuilderPotion,
@@ -114,12 +115,13 @@ public class NPCshop : GlobalNPC {
                 addToShop(shop, ItemID.CombatWrench, Item.buyPrice(0, 10, 0, 0));
                 break;
             case NPCID.Clothier:
-                addToShop(shop, ItemID.GoldenKey, Item.buyPrice(0, 5, 0, 0), () => Main.hardMode);
+                addToShop(shop, ItemID.GoldenKey, Condition.Hardmode, Item.buyPrice(0, 5, 0, 0));
                 break;
             case NPCID.Painter:
                 addToShop(shop, ItemID.PainterPaintballGun, price: Item.buyPrice(0, 15, 0, 0));
                 break;
             case NPCID.Steampunker:
+                // TODO: add proper conditions and lockeys for this
                 addToShop(shop, ItemID.PurpleSolution,
                     Item.buyPrice(0, 0, 5, 0), () => Main.LocalPlayer.ZoneGraveyard && WorldGen.crimson);
                 addToShop(shop, ItemID.RedSolution,
@@ -135,16 +137,16 @@ public class NPCshop : GlobalNPC {
                 addToShop(shop, ItemID.PotionOfReturn,
                     Item.buyPrice(0, cost, 0, 0));
                 addToShop(shop, ItemID.MagicMissile, Item.buyPrice(0, 5, 0, 0));
-                addToShop(shop, ItemID.RodofDiscord,
-                    Item.buyPrice(10, 0, 0, 0), () => Main.hardMode && Main.LocalPlayer.ZoneHallow);
-                addToShop(shop, ItemID.SpectreStaff,
-                    Item.buyPrice(0, 25, 0, 0), () => NPC.downedGolemBoss);
-                addToShop(shop, ItemID.InfernoFork,
-                    Item.buyPrice(0, 25, 0, 0), () => NPC.downedGolemBoss);
-                addToShop(shop, ItemID.ShadowbeamStaff,
-                    Item.buyPrice(0, 25, 0, 0), () => NPC.downedGolemBoss);
-                addToShop(shop, ItemID.MagnetSphere,
-                    Item.buyPrice(0, 25, 0, 0), () => NPC.downedGolemBoss);
+                addToShop(shop, ItemID.RodofDiscord, Condition.InHallow,
+                    Item.buyPrice(10, 0, 0, 0));
+                addToShop(shop, ItemID.SpectreStaff, Condition.DownedGolem,
+                    Item.buyPrice(0, 25, 0, 0));
+                addToShop(shop, ItemID.InfernoFork, Condition.DownedGolem,
+                    Item.buyPrice(0, 25, 0, 0));
+                addToShop(shop, ItemID.ShadowbeamStaff, Condition.DownedGolem,
+                    Item.buyPrice(0, 25, 0, 0));
+                addToShop(shop, ItemID.MagnetSphere, Condition.DownedGolem,
+                    Item.buyPrice(0, 25, 0, 0));
                 break;
             case NPCID.WitchDoctor:
                 addToShop(shop, ItemID.SummoningPotion,
@@ -157,15 +159,15 @@ public class NPCshop : GlobalNPC {
                     Item.buyPrice(0, cost, 0, 0));
                 addToShop(shop, ItemID.InfernoPotion,
                     Item.buyPrice(0, 2, 0, 0));
-                addToShop(shop, ItemID.ButterflyDust,
-                    Item.buyPrice(0, 10, 0, 0), () => NPC.downedGolemBoss);
+                addToShop(shop, ItemID.ButterflyDust, Condition.DownedGolem,
+                    Item.buyPrice(0, 10, 0, 0));
                 addToShop(shop, ItemID.FriedEgg, Item.buyPrice(0, 2, 50, 0));
                 break;
             case NPCID.PartyGirl:
                 addToShop(shop, ItemID.GenderChangePotion,
                     Item.buyPrice(0, 1, 0, 0));
-                addToShop(shop, ItemID.Pizza,
-                    Item.buyPrice(0, 5, 0, 0), () => NPC.downedBoss3);
+                addToShop(shop, ItemID.Pizza, Condition.DownedSkeletron,
+                    Item.buyPrice(0, 5, 0, 0));
                 addToShop(shop, ItemID.CreamSoda, Item.buyPrice(0, 2, 50, 0));
                 break;
             case NPCID.Princess:
@@ -187,7 +189,7 @@ public class NPCshop : GlobalNPC {
                 break;
             // Zoologist
             case NPCID.BestiaryGirl:
-                addToShop(shop, ItemID.Steak, Item.buyPrice(0, 5, 0, 0), () => Main.hardMode);
+                addToShop(shop, ItemID.Steak, Condition.Hardmode, Item.buyPrice(0, 5, 0, 0));
                 break;
         }
     }
@@ -199,19 +201,34 @@ public class NPCshop : GlobalNPC {
     /// <param name="itemID">Which (vanilla) itemID to add.</param>
     /// <param name="price">What the sell price should be.</param>
     /// <param name="cond">The condition to be registered for selling.</param>
-    private void addToShop(NPCShop shop, int itemID, int? price = null, Func<bool>? cond = null) {
-
+    /// <param name="description">The description to use for the condition.</param>
+    private void addToShop(NPCShop shop, int itemID, int? price = null, Func<bool>? cond = null,
+        LocalizedText? description = null) {
         if (!QOLConfig.Instance.sellAdditionalItems) {
             return;
         }
-        
+
+        if (description == null) {
+            description = LocalizedText.Empty;
+        }
+
         // null means always true
         cond ??= () => true;
 
         var shopItem = new Item(itemID) {
             shopCustomPrice = price
         };
-        // TODO: this needs proper conditions for mod support
-        shop.Add(shopItem, new Condition(LocalizedText.Empty, cond));
+        shop.Add(shopItem, new Condition(description, cond));
+    }
+
+    private void addToShop(NPCShop shop, int itemID, Condition cond, int? price = null) {
+        if (!QOLConfig.Instance.sellAdditionalItems) {
+            return;
+        }
+
+        var shopItem = new Item(itemID) {
+            shopCustomPrice = price
+        };
+        shop.Add(shopItem, cond);
     }
 }
